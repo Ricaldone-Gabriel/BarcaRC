cbe697f3832be973704efaae037dc8c5ebfbe9d1
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2023-12-01 16:42:40 +0100

    modifica comandi

--- sketch_nov22a/sketch_nov22a.ino
+++ sketch_nov22a/sketch_nov22a.ino
@@ -1,4 +1,6 @@
 #include <Ps3Controller.h>
+#include "RF24.h"
+
 #define CE_PIN 5
 #define CSN_PIN 13
 // instantiate an object for the nRF24L01 transceiver
@@ -13,19 +15,14 @@ uint8_t address[][6] = { "1Node", "2Node" };
 // It is very helpful to think of an address as a path instead of as
 // an identifying device destination
 bool radioNumber = 0;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
-// Used to control whether this node is sending or receiving
-bool role = true;  // true = TX role, false = RX role
 
-struct payload {
-  short int motore = 0;
-  short int angoloServo = 0;
-}
+short int payload[2] = {0,0}; //0 = Potenza motore, 1 angolo servo
 
 void setup()
 {
     Serial.begin(115200);
     Ps3.begin("a0:5a:5a:a0:09:67");
-    if (!radio.begin()) {
+    /*if (!radio.begin()) {
       Serial.println(F("radio hardware is not responding!!"));
       while (1) {}  // hold in infinite loop
     }
@@ -36,28 +33,24 @@ void setup()
     radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
-    radio.startListening();
+    radio.startListening();*/
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Ps3.isConnected()){
-    if( Ps3.data.button.cross ){
-            Serial.println("Pressing the cross button");
-        }
+    if( abs(Ps3.event.analog_changed.stick.lx) + abs(Ps3.event.analog_changed.stick.ly) > 2 ){
+       Serial.print("Moved the left stick:");
+       Serial.print(" x="); Serial.print(Ps3.data.analog.stick.lx, DEC);
+       Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ly, DEC);
+       Serial.println();
+    }
 
-        if( Ps3.data.button.square ){
-            Serial.println("Pressing the square button");
-        }
-
-        if( Ps3.data.button.triangle ){
-            Serial.println("Pressing the triangle button");
-        }
-
-        if( Ps3.data.button.circle ){
-            Serial.println("Pressing the circle button");
-        }
+    if( abs(Ps3.event.analog_changed.stick.rx) + abs(Ps3.event.analog_changed.stick.ry) > 2 ){
+       Serial.print("Moved the right stick:");
+       Serial.print(" x="); Serial.print(Ps3.data.analog.stick.rx, DEC);
+       Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ry, DEC);
+       Serial.println();
+   }
   }
-
-  delay(3000);
 }
