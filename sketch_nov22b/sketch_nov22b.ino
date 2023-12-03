9e0aae47264e2a78a2b459c0e167b4e88d260463
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2023-12-03 23:17:59 +0100

    Continuazione

--- sketch_nov22b/sketch_nov22b.ino
+++ sketch_nov22b/sketch_nov22b.ino
@@ -1,9 +1,12 @@
 #include <Ps3Controller.h>
+#include <ESP32Servo.h>
+#include <SPI.h>
 #include "RF24.h"
 
 #define CE_PIN 5
 #define CSN_PIN 13
 // instantiate an object for the nRF24L01 transceiver
+Servo Servo1, Servo2, ESC;
 RF24 radio(CE_PIN, CSN_PIN);
 
 //a0:5a:5a:a0:09:67
@@ -14,7 +17,7 @@ RF24 radio(CE_PIN, CSN_PIN);
 uint8_t address[][6] = { "1Node", "2Node" };
 // It is very helpful to think of an address as a path instead of as
 // an identifying device destination
-bool radioNumber = 0;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+bool radioNumber = 1;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
 
 short int payload[2] = {0,90}; //0 = Potenza motore, 1 angolo servo
 
@@ -33,26 +36,19 @@ void setup()
     radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
-    //radio.startListening();
+    radio.startListening();
+
+    Servo1.attach(2);
+    Servo2.attach(3);
+
+    ESC.attach(6,1000,2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //short int payload[2] = {0,0}; //0 = Potenza motore, 1 angolo servo
-  if (Ps3.isConnected()){
-    if( abs(Ps3.event.analog_changed.stick.lx) + abs(Ps3.event.analog_changed.stick.ly) > 2 ){
-       Serial.print("Moved the left stick:");
-       Serial.print(" x="); Serial.print(Ps3.data.analog.stick.lx, DEC);
-       Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ly, DEC);
-       payload[0] = map(Ps3.data.analog.stick.ly,0,127,70,110);
-    }
-
-    if( abs(Ps3.event.analog_changed.stick.rx) + abs(Ps3.event.analog_changed.stick.ry) > 2 ){
-       Serial.print("Moved the right stick:");
-       Serial.print(" x="); Serial.print(Ps3.data.analog.stick.rx, DEC);
-       Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ry, DEC);
-       payload[1] = map(Ps3.data.analog.stick.rx,-128,127,70,110);
-   }
-   radio.write(&payload, sizeof(payload));
+  if(radio.available()) {
+    radio.read(&payload,sizeof(payload));
+    Servo1.write(payload[1]);
+    Servo2.write(payload[1]);
+    ESC.write(payload[0]);
   }
 }
